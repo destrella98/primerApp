ce87860de22023674ec5b82d7ccf3d6db930c69f
destrella <destrella@grupomas.com>
2022-05-11 09:10:39 -0500

    Mejora de calculadora completada

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -7,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
+  operation:string="";
   num1:number=0;
   num2:number=0;
   result:number=0;
 
   constructor() { }
+
+  append(event:Event):void{
+    this.operation= this.operation + (<HTMLInputElement>event.target).value;
+  }
+
+  clear():void{
+    this.operation="";
+  }
 
   add():void{
     this.result=this.num1+this.num2;
@@ -27,6 +36,12 @@ export class CalculadoraComponent implements OnInit {
 
   divide():void{
     this.result=this.num1/this.num2;
+  }
+
+  equal():void{
+    this.result=eval(this.operation);
+    this.operation=""+ this.result;
+
   }
 
   ngOnInit(): void {
